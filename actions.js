ff6762707656f26a38f645eeedbdd2455acaa6d4
Ethan <ethan_rosenfeld@horacemann.org>
2025-06-12 14:20:58 -0400

    Delete actions.js

--- actions.js
+++
@@ -1,8 +0,0 @@
-function show_hide(num) {
-  var x = document.getElementById("hide_text" + num);
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-} 
