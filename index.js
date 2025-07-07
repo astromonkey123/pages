45564c5a61a6a2b0ac095caacccb7bf4b5e71b57
Ethan <ethan_rosenfeld@horacemann.org>
2025-07-06 22:39:24 -0400

    Initial docs page setup

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const windowWidth = window.innerWidth;
+const windowHeight = window.innerHeight;
+    
 // Move the navbar underline to the selected option from the previous option
 window.addEventListener("DOMContentLoaded", () => {
     const defaultItem = document.getElementById("home");
@@ -34,6 +37,7 @@ let prevScrollY = window.scrollY;
 window.addEventListener('scroll', ()=>{
     const nav = document.getElementById('navbar');
     const context = document.getElementsByClassName('context')[0];
+    const pdf_viewer = document.getElementById("pdv");
 
     const currentScrollY = window.scrollY;
 
@@ -45,6 +49,9 @@ window.addEventListener('scroll', ()=>{
                 context.classList.remove("context-expanded");
             }
         }
+        if (pdf_viewer != null) {
+            pdf_viewer.classList.add("pdf-viewer-shadow");
+        }
     } else {
         nav.classList.remove("navbar-shadow");
         if (context != null) {
@@ -52,6 +59,9 @@ window.addEventListener('scroll', ()=>{
             if (prevScrollY > 0) {
                 context.classList.add("context-expanded");
             }
+        }
+        if (pdf_viewer != null) {
+            pdf_viewer.classList.remove("pdf-viewer-shadow");
         }
     }
 
@@ -78,4 +88,67 @@ function showHideContext(id) {
     const element = document.getElementById(id)
 
     element.classList.toggle("context-expanded");
+}
+
+// Show the viewer and load the selected pdf
+function showViewer(id, filename) {
+    const element = document.getElementById(id);
+
+    element.classList.remove("pdf-viewer-hide");
+    element.style.transitionDuration = 0.4 + "s";
+
+    const viewer = element.children[2]
+
+    viewer.style.transitionDuration = 0.2 + "s";
+    viewer.style.opacity = 0;
+    setTimeout(() => {
+        viewer.data = "assets/docs/" + filename;
+    }, 200);
+    setTimeout(() => {
+        viewer.style.opacity = 1;
+    }, 300);
+}
+
+// Hide the viewer
+function hideViewer(id) {
+    const element = document.getElementById(id);
+
+    element.classList.add("pdf-viewer-hide");
+    element.style.transitionDuration = 0.4 + "s";
+}
+
+// Animate the dragging of the viewer
+function dragElement(id) {
+    const element = document.getElementById(id);
+    let mouseUp = false;
+    
+    let x = 0;
+    let y = 0;
+
+    window.addEventListener('mouseup', ()=>{
+        mouseUp = true;
+    });
+
+    window.addEventListener('mousemove', (mouse)=>{
+        x = windowWidth - mouse.clientX;
+        y = mouse.clientY;
+        x = (x < 300) ? 300 : (x > windowWidth - 300) ? windowWidth - 300 : x;
+        y = (y < 25) ? 25 : (y > windowHeight - 500 + 25) ? windowHeight - 500 + 25 : y;
+
+        if (! mouseUp && element != null) {
+            console.log(`${x}, ${y}`);
+            element.style.right = x - 300 + "px";
+            element.style.top = y - 25 + "px";
+            element.style.transitionDuration = 0 + "s";
+        }
+    });
+}
+
+// Reset the pdf viewer's position when double clicked
+function resetElement(id) {
+    const element = document.getElementById(id);
+
+    element.style.right = 25 + "px";
+    element.style.top = 75 + "px";
+    element.style.transitionDuration = 0.6 + "s";
 }
