eaf0ece3ee543eff10b770f7732d4e08cf53a1de
contraflux <eg.rosenfeld@icloud.com>
2025-09-13 20:34:38 -0400

    Massive website redesign

---
+++ index.js
@@ -0,0 +1,26 @@
+const body = document.getElementById("body");
+const navbar = document.getElementById("navbar");
+
+// Fade-in effect on page load
+window.addEventListener("DOMContentLoaded", () => {
+    body.style.opacity = 1;
+});
+
+// Navbar hiding
+document.addEventListener("wheel", (e) => {
+    // At the top, the navbar should be fixed
+    if (window.scrollY == 0) {
+        navbar.style.position = "absolute";
+        navbar.style.top = 0 + "px";
+    // Otherwise it depends on scroll direction
+    } else {
+        navbar.style.position = "fixed";
+        // If scrolling down, hide the navbar
+        if (e.deltaY > 0 && window.scrollY > 60) {
+            navbar.style.top = -60 + "px";
+        // If scrolling up, show the navbar
+        } else {
+            navbar.style.top = 0 + "px";
+        }
+    }
+})
---
+++ projects/matrices/js/core/app.js
@@ -0,0 +1,188 @@
+import { GridContainer} from "../components/Container.js"
+
+import { Matrix } from "../components/Matrix.js";
+import { Vector } from "../components/Vector.js";
+
+import { pixelsToCoords, coordsToPixels, hexToRGB } from "../util/conversions.js";
+import { log, colorLerp } from "../util/utilities.js";
+import { rainbow, highlight, light, dark } from "../util/colors.js";
+
+export const gridContainer = new GridContainer('canvas');
+export const dt = 1e-2;
+
+const canvas = gridContainer.canvas;
+const ctx = gridContainer.ctx;
+
+const a11_input = document.getElementById('a11');
+const a12_input = document.getElementById('a12');
+const a21_input = document.getElementById('a21');
+const a22_input = document.getElementById('a22');
+const invert_input = document.getElementById('invert');
+const transpose_input = document.getElementById('transpose');
+const rank_output = document.getElementById('rank');
+const trace_output = document.getElementById('trace');
+const determinant_output = document.getElementById('determinant');
+const polynomial_output = document.getElementById('characteristic-polynomial');
+const eigval_1_output = document.getElementById('eigenvalue-1');
+const eigval_2_output = document.getElementById('eigenvalue-2');
+const eigvec_1_output = document.getElementById('eigenvector-1');
+const eigvec_2_output = document.getElementById('eigenvector-2');
+
+function drawGrid(v1, v2, color, width) {
+    const lowerLeftBound = pixelsToCoords(0, canvas.height);
+    const upperRightBound = pixelsToCoords(canvas.width, 0);
+
+    const gridSpacing = Math.pow(5, Math.ceil(log(50 / gridContainer.coordScale, 5)));
+
+    const [x_min, y_min] = lowerLeftBound;
+    const [x_max, y_max] = upperRightBound;
+
+    const M = new Matrix(v1.x, v2.x, v1.y, v2.y);
+    const b1 = new Vector(x_min, y_min);
+    const b2 = new Vector(x_min, y_max);
+    const b3 = new Vector(x_max, y_min);
+    const b4 = new Vector(x_max, y_max);
+
+    const M_inv = M.getInverse();
+    const w1 = M_inv.multiply(b1);
+    const w2 = M_inv.multiply(b2);
+    const w3 = M_inv.multiply(b3);
+    const w4 = M_inv.multiply(b4);
+
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+    ctx.font = "18px serif";
+
+    const xs = [w1.x, w2.x, w3.x, w4.x];
+    const ys = [w1.y, w2.y, w3.y, w4.y];
+
+    for (let x = Math.floor(Math.min(...xs) / gridSpacing) * gridSpacing; x <= Math.max(...xs); x += gridSpacing) {
+        for (let y = Math.floor(Math.min(...ys) / gridSpacing) * gridSpacing; y <= Math.max(...ys); y += gridSpacing) {
+            const position = M.multiply(new Vector(x, y));
+
+            ctx.save();
+            ctx.translate(...coordsToPixels(position.x, position.y));
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(v1.x * gridContainer.coordScale * gridSpacing, -v1.y * gridContainer.coordScale * gridSpacing)
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.moveTo(0, 0);
+            ctx.lineTo(v2.x * gridContainer.coordScale * gridSpacing, -v2.y * gridContainer.coordScale * gridSpacing)
+            ctx.stroke();
+
+            if (x == 0) {
+                ctx.fillText(y.toFixed(1), 10, 10);
+            } else if (y == 0) {
+                ctx.fillText(x.toFixed(1), 10, 10);
+            }
+
+            ctx.restore();
+        }
+    }
+}
+
+function drawVector(v, color, width, drawArrow) {
+    const [w_0, h_0] = coordsToPixels(0, 0);
+    const [w, h] = coordsToPixels(...v.asArray());
+    
+    ctx.strokeStyle = color;
+    ctx.lineWidth = width;
+    ctx.font = "18px serif";
+    ctx.save();
+    ctx.beginPath();
+    ctx.moveTo(w_0, h_0);
+    ctx.lineTo(w, h);
+    ctx.stroke();
+    ctx.restore();
+
+    if (drawArrow) {
+        ctx.save();
+        ctx.translate(w, h);
+        ctx.rotate(Math.atan2(v.x, v.y) + Math.PI);
+        ctx.moveTo(-5, -5);
+        ctx.lineTo(0, 0);
+        ctx.lineTo(5, -5);
+        ctx.stroke();
+        ctx.restore();
+    }
+}
+
+function appPeriodic() {
+    gridContainer.matrix.a11 = parseFloat(a11_input.value);
+    gridContainer.matrix.a12 = parseFloat(a12_input.value);
+    gridContainer.matrix.a21 = parseFloat(a21_input.value);
+    gridContainer.matrix.a22 = parseFloat(a22_input.value);
+
+    const [c1, u1, c2, u2] = updateProperties();
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = light;
+    ctx.strokeStyle = light;
+    ctx.font = "12px serif";
+
+    const e1 = new Vector(1, 0);
+    const e2 = new Vector(0, 1);
+
+    const v1 = gridContainer.matrix.multiply(e1);
+    const v2 = gridContainer.matrix.multiply(e2);
+
+    drawGrid(e1, e2, "white", 0.5);
+    drawGrid(v1, v2, "purple", 1);
+
+    drawVector(e1, "white", 2, true);
+    drawVector(e2, "white", 2, true);
+
+    drawVector(v1, "purple", 4, true);
+    drawVector(v2, "purple", 4, true);
+
+    drawVector(u1, "green", 3, true);
+    drawVector(u2, "green", 3, true);
+
+    drawVector(u1._scale(c1), "green", 2, true);
+    drawVector(u2._scale(c2), "green", 2, true);
+}
+
+function updateValues() {
+    a11_input.value = gridContainer.matrix.a11;
+    a12_input.value = gridContainer.matrix.a12;
+    a21_input.value = gridContainer.matrix.a21;
+    a22_input.value = gridContainer.matrix.a22;
+}
+
+function updateProperties() {
+    const [eigval_1, eigval_2] = gridContainer.matrix.eigenvalues();
+
+    rank_output.innerHTML = gridContainer.matrix.rank();
+    trace_output.innerHTML = gridContainer.matrix.trace();
+    determinant_output.innerHTML = gridContainer.matrix.determinant();
+    polynomial_output.innerHTML = gridContainer.matrix.characteristicPolynomial();
+    eigval_1_output.innerHTML = eigval_1;
+    eigval_2_output.innerHTML = eigval_2;
+    const u1 = gridContainer.matrix.eigenvector(eigval_1);
+    const u2 = gridContainer.matrix.eigenvector(eigval_2);
+    eigvec_1_output.innerHTML = u1.asString();
+    eigvec_2_output.innerHTML = u2.asString();
+
+    return [eigval_1, u1, eigval_2, u2];
+}
+
+canvas.addEventListener('mousedown', (e) => { gridContainer.isDragging = true; });
+canvas.addEventListener('mousemove', (e) => gridContainer.dragGrid(e));
+canvas.addEventListener('mouseup', () => { gridContainer.isDragging = false; })
+canvas.addEventListener('wheel', (e) => gridContainer.zoomGrid(e));
+
+invert_input.addEventListener('click', () => {
+    gridContainer.matrix.invert();
+    updateValues();
+});
+transpose_input.addEventListener('click', () => {
+    gridContainer.matrix.transpose();
+    updateValues();
+});
+
+document.addEventListener('keypress', (e) => {
+    if (e.key == 'r') gridContainer.resetFields();
+});
+
+setInterval(appPeriodic, 10);
